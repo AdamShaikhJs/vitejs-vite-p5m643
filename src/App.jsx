14af01245ad194d0046ecaf26531523efc6b43a9
AdamShaikhJs <adamhshaikh2018@gmail.com>
2024-11-22 20:50:56 +0530

    Trace_react_interview

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import ApexCharts from 'apexcharts';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  const fetchData = async () => {
+    let res = await fetch("https://jsonplaceholder.typicode.com/todos")
+    res = await res.json()
+    setData(res)
+    console.log(res[0])
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>fetch data</h1>
+      <div style={{ width: "500px", height: "200px" }}>
+        {
+          data.map((val) => (
+            <div>
+              <label>{val.title}</label>
+              <input type="checkbox" checked={val.completed} />
+            </div>
+
+          ))
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
