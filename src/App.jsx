97f9804f7a8642d6233265651b3e10bf28fc7991
AdamShaikhJs <adamhshaikh2018@gmail.com>
2025-01-24 19:11:26 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
 
 
-import React, { useEffect, useReducer } from "react";
-import { useState } from "react";
+import React, { useState,useEffect, useReducer } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(1);
-  const [startTim, setStartTim] = useState(false)
-
-  const startTimer = () => {
-    setStartTim(true)
-  }
-  const stopTimer = () => {
-    setStartTim(false)
-  }
-  const resetTimer = () => {
-    setStartTim(false)
-    setCount(0)
-  }
-
-  useEffect(() => {
-    let isTimer;
-    if (startTim) {
-      isTimer= setInterval(() => {
-        setCount((pre)=>pre+1)
-      },1000)
-    }
-   return ()=>clearInterval(isTimer)
-  }, [startTim])
-
+  
   return (
     <>
-      <h1>Timer : {count}</h1>
-      <button onClick={startTimer}>start</button>
-      <button onClick={stopTimer}>stop</button>
-      <button onClick={resetTimer}>reset</button>
+     <h1>app</h1>
 
     </>
   )
